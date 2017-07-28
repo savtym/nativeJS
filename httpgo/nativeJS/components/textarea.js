4ed1774b662be0a5448ccbf896938145a51b3b8c
savtym <savtym@gmail.com>
2017-07-28 16:02:04 +0300

    [ADD] modules for httpgo

---
+++ httpgo/nativeJS/components/textarea.js
@@ -0,0 +1,34 @@
+
+export class TextArea {
+
+  static get className() {
+    return 'c-app-textarea';
+  }
+
+
+  /*
+   *   set default value
+   */
+
+  static setDefaultAttr(component, attr) {
+
+    component = Native.findAncestorByClass(component, this.className);
+
+    if (component) {
+      ParseJSON.insertDataToAttrSetText(component, attr);
+    } else {
+      throw new SyntaxError(`Данные некорректны, поле textarea`);
+    }
+
+  }
+
+
+  /*
+   *    set value
+   */
+
+  static addAttrToComponent(component, value) {
+    component.textContent = value;
+  }
+
+}
---
+++ httpgo/nativeJS/router.js
@@ -0,0 +1,49 @@
+
+let currentLink = '';
+
+export class Router {
+  constructor() {
+  }
+
+  static start() {
+
+    this.urls = urls;
+    Parse.start(document.body);
+    this.routing(window.location.pathname, true);
+
+    window.onpopstate = (obj) => {
+      Parse.setStateHistoryComponents();
+      if (obj && obj.state) {
+        Parse.setComponent(obj.state.component);
+      } else {
+        Parse.setComponent('', true);
+      }
+      this.routing(document.location.pathname, true);
+      Observer.emit(Variables.documentIsReady);
+    };
+
+  }
+
+  static routing(url, isHistoryBack = false) {
+
+    if (url !== currentLink) {
+
+      currentLink = url;
+      const curURL = this.urls[url];
+      if (curURL) {
+        document.title = curURL.title;
+        console.log(curURL);
+
+      } else {
+        console.log('/404');
+      }
+
+      if (!isHistoryBack) {
+        history.pushState({ url: url, component: Parse.mainContent.innerHTML }, '', url);
+      }
+
+    }
+  }
+
+
+}
